330d4f83aa4eccd32f7738280131f94299cf0818
Muhammad Shayan <shayan.javed091@gmail.com>
2026-07-16 01:59:40 +0500

    Updated...

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,32 @@
-import app from '../server';
-export default app;
+import express from 'express';
+
+const diagnosticApp = express();
+
+let realApp: any = null;
+let initError: any = null;
+
+try {
+  const serverModule = await import('../server.js');
+  realApp = serverModule.default;
+} catch (err: any) {
+  initError = {
+    message: err.message,
+    stack: err.stack,
+    code: err.code,
+  };
+}
+
+diagnosticApp.use((req, res, next) => {
+  if (initError) {
+    return res.status(500).json({
+      error: 'Server initialization failed',
+      details: initError,
+    });
+  }
+  if (realApp) {
+    return realApp(req, res, next);
+  }
+  res.status(500).json({ error: 'App not initialized' });
+});
+
+export default diagnosticApp;
