14a676239dc63bbb9978320451d8b6106157bed9
Muhammad Shayan <shayan.javed091@gmail.com>
2026-07-16 02:50:04 +0500

    Updated...

--- api/index.ts
+++ api/index.ts
@@ -1,21 +1,2 @@
-import express from 'express';
-
-const app = express();
-
-app.all('*', async (req, res, next) => {
-  try {
-    const serverModule = await import('../server');
-    const realApp = serverModule.default;
-    return realApp(req, res, next);
-  } catch (err: any) {
-    res.status(500).json({
-      error: 'Vercel Lambda Runtime Crash',
-      message: err.message,
-      stack: err.stack,
-      name: err.name,
-      code: err.code
-    });
-  }
-});
-
+import app from '../server';
 export default app;
